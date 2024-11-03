57066401472fd5c4d1feb519c3b2639ea6fe5904
alimarf <alimarief97@gmail.com>
2024-11-03 22:41:10 +0700

    fix: error user model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  phone: { type: String, required: true },
-  address: { type: String, required: true }
-}, {
-  toJSON: {
-    transform: function (doc, ret) {
-      delete ret.password;
-      delete ret.__v;
-      return ret;
-    }
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+        return ret;
+      },
+    },
   }
-});
+);
 
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

1f7eea54ccf520497ec9c10244304591a337ee83
alimarf <alimarief97@gmail.com>
2024-11-03 22:31:17 +0700

    fix: remove password and __v from response

--- models/User.js
+++ models/User.js
@@ -1,11 +1,15 @@
-const mongoose = require("mongoose");
-
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String, required: true },    
+  phone: { type: String, required: true },
   address: { type: String, required: true }
+}, {
+  toJSON: {
+    transform: function (doc, ret) {
+      delete ret.password;
+      delete ret.__v;
+      return ret;
+    }
+  }
 });
-
-module.exports = mongoose.model("User", userSchema);
